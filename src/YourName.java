382435fab1691472b64a878e7e5814773ee75d9e
Edward McLevige <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-06 11:28:16 -0500

    Initial commit

---
+++ src/YourName.java
@@ -0,0 +1,6 @@
+public class YourName {
+    public static void main(String[] args){
+        System.out.println("Eddie Mclevige");
+
+    }
+}
---
+++ src/prod54b.java
@@ -0,0 +1,5 @@
+public class prod54b {
+    public static void main(String[] args){
+       
+    }
+}
