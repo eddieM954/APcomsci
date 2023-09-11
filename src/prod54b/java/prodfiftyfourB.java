1362a025e078e64bd1e79e7caaa49cfbf0c71008
mclevige.e1 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-11 13:01:46 -0500

    Update

---
+++ src/prod54b/java/prodfiftyfourB.java
@@ -0,0 +1,26 @@
+package prod54b.java;
+
+import java.util.*;
+
+public class prodfiftyfourB {
+    public static void main(String[] args) {
+        Scanner Keyboard = new Scanner(System.in);
+
+
+        System.out.print("What is the first number?");
+        int num1 = Keyboard.nextInt();
+
+        System.out.print("What is the second number? ");
+        int num2 = Keyboard.nextInt();
+        System.out.print("What is the third number? ");
+        int num3 = Keyboard.nextInt();
+        System.out.print("What is the fourth number? ");
+        int num4 = Keyboard.nextInt();
+        int sum = num1 + num2 + num3 + num4;
+        System.out.print("The sum of the four numbers is " + sum);
+
+    }
+
+
+
+}
