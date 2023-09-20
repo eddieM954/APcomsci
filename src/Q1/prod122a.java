5e38e15830ca6c6c4bfffad04f46e34aa19ba032
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-20 11:33:01 -0500

    work

---
+++ src/Q1/prod122a.java
@@ -0,0 +1,21 @@
+package Q1;
+
+public class prod122a {
+    public static void main(String[] args){
+        int num1 = 0;
+        while (num1<50){
+            num1 += 1;
+            num1 *= num1;
+            System.out.println(num1 + "     "  );
+
+
+
+        }
+
+
+        }
+
+
+    }
+
+
