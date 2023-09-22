c0e8620ff40afecc0d26bae8c775029cae89fbc0
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-22 13:38:33 -0500

    work

---
+++ src/Q1/prog122c.java
@@ -0,0 +1,13 @@
+package Q1;
+
+public class prog122c {
+    public static void main(String[] args){
+        int num = 0;
+        int num2 = 1;
+        while (num < 10){
+            num += 2;
+            num2 += 2;
+            System.out.println(num + num2);
+        }
+    }
+}
