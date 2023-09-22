9e082900660fdd9d5e9dbc6f9ce579dc4bde378f
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-22 13:19:57 -0500

    work

--- src/Q1/prod122a.java
+++ src/Q1/prod122a.java
@@ -4,12 +4,15 @@ public class prod122a {
     public static void main(String[] args){
         int num1 = 0;
         int num2 = 1;
-        int num4 = Math.sqrt(num1);
+        System.out.println("Number       Square     Square Root");
         while (num1<50){
             num1 += 1;
-            double num3 = Math.pow(num1, 2);
+            int num3 = (int)Math.pow(num1, 2);
+            double num4 = Math.sqrt(num1);
 
-            System.out.println(num1 + "     " +  num3  );
+
+
+            System.out.printf( num1 + "             " + num3 + "         %.4f\n", + num4);
 
 
             }
@@ -26,7 +29,5 @@ public class prod122a {
 
 
 
-//              while (num2<2500){
-//                Math.pow(num1,2);
-//                System.out.println(num2);
+//              System.out.printf(num1 + "     " +  num3 + "       %.4f\n ", + num4 );
 
