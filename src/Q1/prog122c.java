0a6c70f3959d0a6b84c9125f47df6b15e660a4eb
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-25 11:20:47 -0500

    work

--- src/Q1/prog122c.java
+++ src/Q1/prog122c.java
@@ -4,10 +4,15 @@ public class prog122c {
     public static void main(String[] args){
         int num = 0;
         int num2 = 1;
+        int num3 = 0;
+
         while (num < 10){
+
             num += 2;
             num2 += 2;
-            System.out.println(num + "   "  + num2);
+            num3  += 4;
+            int num4 = (int)Math.pow(num,2 );
+            System.out.println(num + "   "  + num2  + "    " + num3 + "    "  +   num4);
         }
     }
 }
