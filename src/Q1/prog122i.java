bd2f6928d7428413ac9e9b7f62ed6961ad92087a
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-27 13:48:48 -0500

    work

---
+++ src/Q1/prog122i.java
@@ -0,0 +1,19 @@
+package Q1;
+
+
+public class prog122i {
+    public static void main(String[] args) {
+
+        int num = -26;
+
+        while (num< 25){
+            num += 1;
+            double cuber = Math.pow(num, .33);
+
+
+            System.out.println(num + "     " );
+
+
+        }
+    }
+}
