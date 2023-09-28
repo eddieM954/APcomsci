066cd5654c1b49bc24c4bb52144a0fcc7c4e89a9
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-09-28 13:59:32 -0500

    work

---
+++ src/Q1/prog166f.java
@@ -0,0 +1,48 @@
+package Q1;
+
+public class prog166f {
+    public static void main(String[] args) {
+
+        int num = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int num4 = 0;
+        int num5= 0;
+        for (int i= 1; i<101; i ++){
+            num = num + i;
+
+
+
+        }
+        for (int i= 1; i<102; i ++){
+            num2 = num2 + i;
+
+
+
+        }
+        for (int i= 1; i<103; i ++){
+            num3 = num3 + i;
+
+
+
+        }
+        for (int i= 1; i<104; i ++){
+            num4 = num4 + i;
+
+
+
+        }
+        for (int i= 1; i<105; i ++){
+            num5 = num5 + i;
+
+
+
+        }
+
+        System.out.println("The sum of the numbers 1 - 100 is:  " + num);
+        System.out.println("The sum of the numbers 1 - 101 is:  "+ num2);
+        System.out.println("The sum of the numbers 1 - 102 is:  " + num3);
+        System.out.println("The sum of the numbers 1 - 103 is:  "+ num4);
+        System.out.println("The sum of the numbers 1 - 104 is:  "+num5);
+    }
+}
