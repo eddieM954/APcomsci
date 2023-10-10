957fabd4822a0c6925bbe31c353e978a3e841e1c
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-10-10 11:05:12 -0500

    work

--- src/Q1/LP410.java
+++ src/Q1/LP410.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class LP410 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Rectangular Prism");
+        System.out.println("Rectangular Prism");
         System.out.print("Enter the length: ");
         int len = input.nextInt();
         System.out.print("Enter the width:");
@@ -12,14 +12,22 @@ public class LP410 {
         System.out.print("Enter the height:");
         int hei = input.nextInt();
         int volume = len * wid * hei;
-        System.out.print(volume);
+        System.out.print("The volume is " + volume);
         System.out.println("");
 
-        System.out.print("Sphere");
+        System.out.println("Sphere");
         System.out.print("Enter the radius: ");
         int rad = input.nextInt();
         int cir = rad * 2;
         double num = ((int)Math.pow(cir, 3 ) * 3.14159) / 6;
+        System.out.printf("The volume is: %.2f\n", + num);
+
+        System.out.println("Cube");
+        System.out.print("Enter the length of each side: ");
+        int lencu = input.nextInt();
+        int num2 = (int)Math.pow(lencu, 3);
+        System.out.print("The volume is " + num2);
+
 
     }
 }
