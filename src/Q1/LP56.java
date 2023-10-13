51643b5b98b3b6b5a7395585892093895ab5c4c8
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-10-13 10:59:20 -0500

    work

--- src/Q1/LP56.java
+++ src/Q1/LP56.java
@@ -1,20 +1,19 @@
 package Q1;
+
+
+
 import java.util.*;
 
 public class LP56 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter a positive integer: ");
         int num = input.nextInt();
-
-        while (num > 100 && num < 1000){
-            int sum = num%=10;
-            sum /= 10;
-            int sum2 = sum%10;
-            sum2/=10;
-            int sum3 = sum2%10;
-            System.out.print(num + sum + sum2);
+        int sum = 0;
+        while (num > 0){
+            sum += num % 10;
+            num /= 10;
         }
+        System.out.print(sum);
     }
 }
