6f50b0dd55d3407e63e50aef0e2cb4d184e10e46
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-10-24 11:23:37 -0500

    work

--- src/Q1/prog213eCl/Cl213e.java
+++ src/Q1/prog213eCl/Cl213e.java
@@ -10,10 +10,10 @@ public class Cl213e {
     private int myDis;
     private double myPercent;
 
-    public Cl213e(int dis){
-        myAge = 0;
+    public Cl213e(int age){
+        myAge = age;
         myDis = 0;
-        myPercent = 0;
+        myPercent = 1;
         myAgetwenty = 0;
         myAge20to39 = 0;
         myAge40to59 = 0;
@@ -21,14 +21,27 @@ public class Cl213e {
         myAge79nup = 0;
 
 
+
     }
 
     public void calc(){
+        myDis = myAgetwenty + myAge20to39 + myAge40to59 + myAge60to79 + myAge79nup;
         if (myAge < 20 ) myAgetwenty += 1;
-        if (myAge > 19 && myAge <= 39 ) myAge20to39 += 1;
-        if (myAge > 39 && myAge <= 59) myAge40to59 += 1;
-        if (myAge > 59 && myAge <= 79) myAge60to79 += 1;
-        if (myAge > 79) myAge79nup += 1;
-    }
+        else if (myAge > 19 && myAge <= 39 ) myAge20to39 += 1;
+        else if (myAge > 39 && myAge <= 59) myAge40to59 += 1;
+        else if (myAge > 59 && myAge <= 79) myAge60to79 += 1;
+        else if (myAge > 79) myAge79nup += 1;
 
+
+    }
+    public void setComm() {calc();}
+
+
+    public int getMyage(){return myAge; }
+    public int getMyagetwenty(){ return myAgetwenty;}
+
+
+    public String toString(){
+        return String.format("Age Group  Distribution  Percent ", myAgetwenty);
+    }
 }
--- src/Q1/prog213eCl/prog213e.java
+++ src/Q1/prog213eCl/prog213e.java
@@ -1,4 +1,22 @@
 package Q1.prog213eCl;
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
 
 public class prog213e {
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("prog213e.txt"));
+
+            while(input.hasNext()){
+                int myAge = input.nextInt();
+                Cl213e wowza = new Cl213e(myAge);
+                wowza.calc();
+                System.out.print(wowza);
+            }
+        }catch (IOException e){
+            System.out.print("");
+        }
+    }
 }
