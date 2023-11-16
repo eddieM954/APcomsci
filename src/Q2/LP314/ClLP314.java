bbc8881dab9090958981a0fc82b9d0504d6ece8b
eddieM954 <mclevige.e1@stu.janesville.k12.wi.us>
2023-11-16 11:20:22 -0600

    work

--- src/Q2/LP314/ClLP314.java
+++ src/Q2/LP314/ClLP314.java
@@ -7,7 +7,10 @@ public class ClLP314 {
     private int myMarNew;
     private int myMaryJer;
     private int myMarConn;
-    private int myPercent;
+    private double myPercent;
+    private double myPercent2;
+    private int total1;
+    private int total2;
 
     public ClLP314(int awnew, int awjer, int awconn, int marnew, int maryjer, int marconn){
         myAwNew = awnew;
@@ -16,6 +19,21 @@ public class ClLP314 {
         myMarNew = marnew;
         myMaryJer = maryjer;
         myMarConn = marconn;
+        myPercent = 0;
+        total1 = 0;
+        total2 = 0;
+        myPercent2 = 0;
 
+    }
+    public void calc(){
+        total1 = myAwNew + myAwConn + myAwJer;
+        total2 = myMarConn + myMarNew + myMaryJer;
+        myPercent = total1 / (total1 + total2);
+        myPercent2 = total2 / (total1 + total2);
+
+
+    }
+    public String toString(){
+        return String.format("Canidate\tVotes  Percentage\n Awbrey" + total1 + "%.2f" + myPercent + "\nMartinez\t" + total2 + " %.2f" + myPercent2 + "\n Total Votes\t" + (total1 + total2)  );
     }
 }
---
+++ src/Q2/LP314/LP314.java
@@ -0,0 +1,24 @@
+package Q2.LP314;
+import java.util.*;
+
+public class LP314 {
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Election results for New York: ");
+        System.out.print("Awbrey: ");
+        int awnew = input.nextInt();
+        System.out.print("Martinez: ");
+        int marnew = input.nextInt();
+        System.out.println("");
+        System.out.print("Election results for New Jersey: ");
+        System.out.print("Awbrey: ");
+        int awjer = input.nextInt();
+        System.out.print("Martinez: ");
+        int maryjer = input.nextInt();
+        System.out.println("Election results for Connecticut: ");
+
+
+
+    }
+}
