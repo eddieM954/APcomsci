7822405145fa8b7191f5aa6730b6a90db4996aed
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-01-03 13:48:57 -0600

    work

---
+++ src/Q2/prog215i/Cl215i.java
@@ -0,0 +1,22 @@
+package Q2.prog215i;
+
+public class Cl215i {
+    private int myVehicle;
+    private double myMiles;
+    private double myGallons;
+    private double myMPG;
+
+
+    public Cl215i(int vehicle, double miles, double gallons){
+        myVehicle = vehicle;
+        myMiles = miles;
+        myGallons = gallons;
+        myMPG = 0.0;
+    }
+    public void calc(){
+        myMPG = myMiles / myGallons;
+    }
+    public String toString(){
+        return String.format("Vehicle\tMiles\tGallons\tMPG\n%d\t%.2f\t%.2f\t%.1f",myVehicle,myMiles,myGallons,myMPG);
+    }
+}
---
+++ src/Q2/prog215i/prog215i.java
@@ -0,0 +1,31 @@
+package Q2.prog215i;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prog215i {
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("langdat/prog215i.dat"));
+
+            while (input.hasNext()){
+                int vehicle = input.nextInt();
+                double miles = input.nextDouble();
+                double gallons = input.nextDouble();
+                double mpg = 0.0;
+                Cl215i wow = new Cl215i(vehicle, miles, gallons);
+                wow.calc();
+                System.out.println(vehicle + " " + miles + " " + gallons);
+
+
+
+            }
+
+
+        }catch(IOException e){
+            System.out.println("Cant find data file");
+
+        }
+    }
+}
