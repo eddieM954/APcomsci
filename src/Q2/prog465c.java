7445ce30a947d237c5fef2b062319b2b4e5c5d6e
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-01-17 13:50:57 -0600

    work

---
+++ src/Q2/prog465c.java
@@ -0,0 +1,45 @@
+package Q2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prog465c {
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("langdat/Prog465c.txt"));
+            int col = input.nextInt();
+            int row = input.nextInt();
+            int sum = 0;
+            int[][]mat = new int[col][row];
+            for(int r =0; r < mat.length; r++)
+                for(int c = 0; c < mat[r].length; c++)
+                    mat[r][c] = input.nextInt();
+
+            for(int r = 0; r < mat.length; r++){
+                for(int c = 0; c < mat[0].length; c++){
+                    System.out.print(mat[r][c]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+            for(int i = 0; i < mat.length; i++){
+                for(int j = 0; j < mat[i].length;j++){
+                    if(i == 0 || i == mat.length - 1) {
+                        sum += mat[i][j];
+                    }else{
+                        sum += mat[i][mat[i].length - 1 ] +mat[i][0];
+                    }
+                }
+
+            }
+            System.out.print(sum);
+
+
+        }catch(IOException e){
+            System.out.println("Cant find data file");
+
+        }
+    }
+}
