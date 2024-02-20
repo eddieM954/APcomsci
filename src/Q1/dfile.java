49eae3dc4b5a5d859e07354f81494433af565a8f
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-02-20 11:22:30 -0600

    work

--- src/Q1/dfile.java
+++ src/Q1/dfile.java
@@ -3,8 +3,7 @@ package Q1;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class dfile {
     public static void main(String[] args) {
