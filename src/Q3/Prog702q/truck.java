879d74115df8562948f41bd34d6df5a0ac64485c
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-02-26 23:05:04 -0600

    Update truck.java

--- src/Q3/Prog702q/truck.java
+++ src/Q3/Prog702q/truck.java
@@ -1,6 +1,12 @@
 package Q3.Prog702q;
 
-//public class truck extends vehicle{
+public class truck extends vehicle{
+    private int miles;
 
+    public truck(String name, int tires, double value, int miles){
+        super(name, tires, value);
+    }
 
-//}
+    public int getMiles(){return miles;}
+
+}
