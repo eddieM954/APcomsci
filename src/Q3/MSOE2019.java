061fb1c703d40ba633ed23c58cab2eda7ebccb1a
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-04-01 11:22:31 -0500

    work

---
+++ src/Q3/MSOE2019.java
@@ -0,0 +1,14 @@
+package Q3;
+import java.util.Scanner;
+public class MSOE2019 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 2 letters: ");
+        String letters = input.nextLine();
+        System.out.println("Enter a word: ");
+        String line = input.nextLine();
+        for(int lcv = 0; lcv < line.length();lcv++ ){
+
+        }
+    }
+}
