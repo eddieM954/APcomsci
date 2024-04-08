a8264813f8a76daa11a5f3a9ee29cbedd961ed8c
eddieM954 <143827293+eddieM954@users.noreply.github.com>
2024-04-08 11:22:09 -0500

    work

--- src/Q4/MSOE2022.java
+++ src/Q4/MSOE2022.java
@@ -1,8 +1,20 @@
 package Q4;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MSOE2022 {
-    ArrayList<Integer> buckets = new ArrayList<>();
-   // buckets.add(0);
+
+
+    public static void main(String[] args) {
+        ArrayList<String> buckets = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of seashells: ");
+        String line = input.nextLine();
+        buckets.add(line);
+        int cnt = 0;
+        for(int lcv = 0; lcv < buckets.size(); lcv++ ){
+
+        }
+    }
 }
